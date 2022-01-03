9d3f5534453e2727757f5855dc05eaac5f8cc30f
none <null@example.com>
2022-01-02 19:40:01 -0600

    convert program to snake case

--- SyscallDumper/SyscallDumper.cpp
+++ SyscallDumper/SyscallDumper.cpp
@@ -21,36 +21,36 @@
 #include <iomanip>
 
 // Function prototypes
-BOOL IsSyscall(LPCVOID pFunction);
+boolean is_syscall(LPCVOID pFunction);
 
 // Entry point
-DWORD main() {
-    HANDLE hDll = LoadLibraryExA(
+int main() {
+    HANDLE mod_ntdll = LoadLibraryExA(
         "C:\\Windows\\System32\\ntdll.dll", NULL, LOAD_LIBRARY_AS_DATAFILE);
-    if (!hDll || hDll == INVALID_HANDLE_VALUE)
+    if (!mod_ntdll || mod_ntdll == INVALID_HANDLE_VALUE)
         return GetLastError();
 
-    auto pModuleBase = reinterpret_cast<PBYTE>(hDll);
-    auto pDosHeader = reinterpret_cast<PIMAGE_DOS_HEADER>(pModuleBase);
-    if (pDosHeader->e_magic != IMAGE_DOS_SIGNATURE)
+    auto ntdll_base = reinterpret_cast<PBYTE>(mod_ntdll);
+    auto dos_header = reinterpret_cast<PIMAGE_DOS_HEADER>(ntdll_base);
+    if (dos_header->e_magic != IMAGE_DOS_SIGNATURE)
         return ERROR_INVALID_EXE_SIGNATURE;
 
-    auto pNtHeader = reinterpret_cast<PIMAGE_NT_HEADERS>(
-        pModuleBase + pDosHeader->e_lfanew);
-    if (pNtHeader->Signature != IMAGE_NT_SIGNATURE)
+    auto nt_header = reinterpret_cast<PIMAGE_NT_HEADERS>(
+        ntdll_base + dos_header->e_lfanew);
+    if (nt_header->Signature != IMAGE_NT_SIGNATURE)
         return ERROR_INVALID_EXE_SIGNATURE;
 
-    auto pExportDir = reinterpret_cast<PIMAGE_EXPORT_DIRECTORY>(
-        pModuleBase + pNtHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
-    if (pExportDir->NumberOfFunctions == 0)
+    auto export_dir = reinterpret_cast<PIMAGE_EXPORT_DIRECTORY>(
+        ntdll_base + nt_header->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
+    if (export_dir->NumberOfFunctions == 0)
         ERROR_INVALID_DLL;
 
-    auto pdwAddressOfFunc = 
-        reinterpret_cast<PDWORD>((reinterpret_cast<LPBYTE>(pModuleBase) + pExportDir->AddressOfFunctions));
-    auto pdwAddressOfName = 
-        reinterpret_cast<PDWORD>((reinterpret_cast<LPBYTE>(pModuleBase) + pExportDir->AddressOfNames));
-    auto pwAddressOfOrd = 
-        reinterpret_cast<PWORD>((reinterpret_cast<LPBYTE>(pModuleBase) + pExportDir->AddressOfNameOrdinals));
+    auto address_of_func = 
+        reinterpret_cast<PDWORD>((reinterpret_cast<LPBYTE>(ntdll_base) + export_dir->AddressOfFunctions));
+    auto address_of_name = 
+        reinterpret_cast<PDWORD>((reinterpret_cast<LPBYTE>(ntdll_base) + export_dir->AddressOfNames));
+    auto address_of_ord = 
+        reinterpret_cast<PWORD>((reinterpret_cast<LPBYTE>(ntdll_base) + export_dir->AddressOfNameOrdinals));
 
     // Print column headers
     std::cout << std::left
@@ -59,47 +59,44 @@ DWORD main() {
         << std::setw(10) << "number"
         << "name\n" << std::endl;
 
-    for (uint64_t i = 0; i < pExportDir->NumberOfFunctions; i++) {
+    for (uint64_t i = 0; i < export_dir->NumberOfFunctions; i++) {
         // Get the pointer to the function
-        auto pCurrentFunction = (PVOID)(
-            pModuleBase + pdwAddressOfFunc[pwAddressOfOrd[i]]);
+        auto current_function = (PVOID)(
+            ntdll_base + address_of_func[address_of_ord[i]]);
             
         // Identify "Nt" family functions
-        if (IsSyscall(pCurrentFunction)) {           
+        if (is_syscall(current_function)) {           
             // Calculate its RVA
-            DWORD dwRva = (DWORD)pCurrentFunction - pNtHeader->OptionalHeader.ImageBase;
-            auto szFunctionName = (char*)(pModuleBase + pdwAddressOfName[i]);
+            DWORD rva = (DWORD)current_function - nt_header->OptionalHeader.ImageBase;
+            auto function_name = (char*)(ntdll_base + address_of_name[i]);
             
             // Retrieve the syscall code number from the raw bytes
-            auto pFunctionCode = *(uintptr_t*)pCurrentFunction;
-            auto syscallNum = (pFunctionCode >> 8 * 4);
-            syscallNum = syscallNum & 0xfff;
+            auto function_data = *(uintptr_t*)current_function;
+            auto syscall_num = (function_data >> 8 * 4);
+            syscall_num = syscall_num & 0xfff;
                                            
             // Print the function's information
             std::cout << std::left
                 << std::setw(10) << std::dec << i
-                << std::setw(10) << std::hex << dwRva
-                << std::setw(10) << std::hex << syscallNum
-                << szFunctionName << std::endl;            
+                << std::setw(10) << std::hex << rva
+                << std::setw(10) << std::hex << syscall_num
+                << function_name << std::endl;            
         }
     }
-
-    BOOL bStatus = FreeLibrary((HMODULE)hDll);
-    if (!bStatus) 
-        return GetLastError();
     
     return ERROR_SUCCESS;
 }
 
 
 // Check the first four bytes of the function for operations indicating a syscall
-BOOL IsSyscall(LPCVOID pFunction) {
-    LPCBYTE pBytePtr = (LPCBYTE)pFunction;
+boolean is_syscall(LPCVOID function_ptr) {
+    unsigned char* byte_ptr = (unsigned char*)function_ptr;
 
-    if (pBytePtr[0] == 0x4C &&
-        pBytePtr[1] == 0x8B &&
-        pBytePtr[2] == 0xD1 &&
-        pBytePtr[3] == 0xB8) return TRUE;
+    if (byte_ptr[0] == 0x4C &&
+        byte_ptr[1] == 0x8B &&
+        byte_ptr[2] == 0xD1 &&
 
-    return FALSE;
+        byte_ptr[3] == 0xB8) return true;
+
+    return false;
 }
